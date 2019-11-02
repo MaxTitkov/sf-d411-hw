f806e7d62bdb40b94134d72254cec2bced805e10
mt <breddowen@gmail.com>
2019-11-02 19:06:36 +0300

    Heroku files

---
+++ sf_d411/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\soft\~skillfactory_2\mod_d_4\dj_library\sf-d411-hw\sf_d411\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
